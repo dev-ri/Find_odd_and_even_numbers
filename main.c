6382c3975b0123cde0f070c066c578adf282f431
Md Rohidul Islam <rohidul677@gmail.com>
2019-12-23 07:26:32 +0600

    Select starting number according to odd or even

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 int main()
 {
     int min, max, newMin, newMax;
+    char ans;
+
     printf("Input your range(Example: 1-100) : ");
     scanf("%d-%d",&min,&max);
     if(min>max){
@@ -14,5 +16,19 @@ int main()
         newMax = max;
         newMin = min;
     }
-    printf("Range : %d-%d",newMin, newMax);
+    //Now checking what user needs, even numbers or odd numbers!
+    printf("Odd or Even? (o/e) ");
+    scanf("%s",&ans);
+    if(ans == 'o' | ans == 'O'){
+        if(newMin%2 == 0){
+            newMin = newMin + 1;
+        }
+        printf("%d-%d",newMin,newMax);
+    }
+    else if(ans == 'e' | ans == 'E'){
+        if(newMin%2 == 1){
+            newMin = newMin + 1;
+        }
+        printf("%d-%d",newMin,newMax);
+    }
 }
