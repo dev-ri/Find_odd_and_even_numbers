cde97e9a4648999b91e95aae62b66687b74e0294
Md Rohidul Islam <rohidul677@gmail.com>
2019-12-23 07:10:49 +0600

    Input Range
    I added a code for inputing a range and print it on the terminal. This program will going to convert user input number small to big

--- main.c
+++ main.c
@@ -3,5 +3,16 @@
 
 int main()
 {
-
+    int min, max, newMin, newMax;
+    printf("Input your range(Example: 1-100) : ");
+    scanf("%d-%d",&min,&max);
+    if(min>max){
+        newMin = max;
+        newMax = min;
+    }
+    else{
+        newMax = max;
+        newMin = min;
+    }
+    printf("Range : %d-%d",newMin, newMax);
 }
